bac6300fde8b9038220aa6747d7c1eef4a774432
Tamara <tamara.volic@hotmail.com>
2022-05-10 13:47:05 +0200

    ..

--- index.php
+++ index.php
@@ -4,12 +4,9 @@ require 'core/bootstrap.php';
 $routes = [
 	'/creditlist' => 'CreditListController@index',
 	'/createcredit' => 'CreateCreditController@index',
-<<<<<<< HEAD
-	'/home' => 'HomeController@index'
-=======
+	'/home' => 'HomeController@index',
 	'/editcredit' => 'EditCreditController@index',
 	'/validate' => 'ValidationController@index'
->>>>>>> c625101ddcda95fd0d14a08ad87c009373f55dbe
 ];
 
 $db = [
