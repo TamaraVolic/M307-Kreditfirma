cf784b2161eada0f17bece97000fb68b3006d190
robertfodor93 <robert.fodor@sluz.ch>
2022-05-10 09:08:14 +0200

    create credit controller and view added

---
+++ app/Controllers/EditCreditController.php
@@ -0,0 +1,38 @@
+<?php
+require 'app/Controllers/ValidationController.php';
+require 'app/Models/CreditPackageModel.php';
+require 'app/Models/CreditModel.php';
+
+class EditController
+{
+
+    public function index()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $this->edit($_POST);
+        } else {
+            if (isset($_GET['id'])) {
+                $creditpackageModel = new CreditpackageModel();
+                $loanModel = new LoanModel();
+                $creditpackageData = $creditpackageModel->getAll("creditpackage");
+                $result = $loanModel->load("loan", $_GET['id']);
+                require 'app/Views/edit.view.php';
+            } else {
+                http_response_code(422);
+                echo 'Id parameter is required';
+            }
+        }
+    }
+
+
+    public function edit($data)
+    {
+        $validation_result = ValidationController::validateEdit($data);
+        if (count($validation_result) > 0) {
+            http_response_code(422);
+            echo json_encode($validation_result);
+        } else {
+            $result = LoanModel::updateLoan($_GET['id'], $data);
+        }
+    }
+}
