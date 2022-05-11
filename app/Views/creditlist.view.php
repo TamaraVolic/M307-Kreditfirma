a2504c2f4de3c758215fd30db21d0f3de79e484d
Tamara <tamara.volic@hotmail.com>
2022-05-11 11:09:47 +0200

    Merge branch 'main' of https://github.com/TamaraVolic/M307-Kreditfirma

--- app/Views/creditlist.view.php
+++ app/Views/creditlist.view.php
@@ -15,7 +15,7 @@
         <h1 style=" z-index: 1;">Kredithay</h1>
         <i style="float: right;" class="fa-solid fa-user fa-lg"></i>
         <div class="tablecenter">
-            <li><a href="home">Home</a></li>
+            <li><a href="home">Startseite</a></li>
             <li><a href="creditlist">Kredit Übersicht</a></li>
             <li><a href="createcredit">Kredit Erstellen</a></li>
             </ul>
@@ -27,6 +27,20 @@
     <div class="container">
         <h1 class="welcome">Kredit Übersicht</h1>
         <table class="ths">
+        <tr>
+            <th>Vorname</th>
+            <th>Nachname</th>
+            <th>E-Mail</th>
+            <th>Telefonnummer</th>
+            <th>Anzahl der Raten</th>
+            <th>Kredit Paket</th>
+            <th>Rückzahldatum</th>
+            <th>Fällig</th>
+            <th></th>
+        </tr>
+        <?php
+        foreach ($result as $credit) {
+            ?>
             <tr>
                 <td><?= e($credit['first_name']) ?></td>
                 <td><?= e($credit['last_name']) ?></td>
@@ -42,26 +56,8 @@
                     </button>
                 </td>
             </tr>
-            <?php
-            foreach ($result as $credit) {
-            ?>
-                <tr>
-                    <td><?= e($credit['first_name']) ?></td>
-                    <td><?= e($credit['last_name']) ?></td>
-                    <td><?= e($credit['email']) ?></td>
-                    <td><?= isset($credit['phone_number']) ? e($credit['phone_number']) : '' ?></td>
-                    <td><?= e($credit['installments']) ?></td>
-                    <td><?= e($credit['credit_package']) ?></td>
-                    <td><?= e($credit['payback_date']) ?></td>
-                    <td><?= $credit['due'] == 0 ? '&#128309;' : '&#128308;' ?></td>
-                    <td>
-                        <button onclick="window.location.href='<?= ROOT_URL ?>/editcredit?id=<?= $credit['credit_id'] ?>'">&#x270F;&#xFE0F;
-                            edit
-                        </button>
-                    </td>
-                </tr>
-            <?php } ?>
-        </table>
+        <?php } ?>
+    </table>
         <button onclick="window.location.href='<?= ROOT_URL ?>/home'">&#127968; Zurück zur Startseite</button>
         <button onclick="window.location.href='<?= ROOT_URL ?>/createcredit'">&#10133; Neuen Kredit Erstellen</button>
     </div>
